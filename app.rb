10585add1b58c46f7d6a3114501c435e297278ce
Masen Matthews and Maggie Summers <maggie@>
2018-02-19 15:16:07 -0800

    update front-end functionality

--- app.rb
+++ app.rb
@@ -5,3 +5,18 @@ require("./lib/task")
 require("pg")
 
 DB = PG.connect({:dbname => "to_do"})
+
+get('/') do
+  erb(:index)
+end
+
+get("/lists/new") do
+  erb(:list_form)
+end
+
+post("/lists") do
+  name = params.fetch("name")
+  list = List.new({:name => name, :id => nil})
+  list.save()
+  erb(:list_success)
+end
